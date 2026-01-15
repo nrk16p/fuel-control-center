c8932a45a8dffa5809cbbada238cad127708562d
Menatransport_02 <menatransport_02@Menatransport-02s-MacBook-Air.local>
2026-01-15 13:52:40 +0700

    api for fuel

--- src/app/api/engineon/route.ts
+++ src/app/api/engineon/route.ts
@@ -17,6 +17,27 @@ export async function GET(request: Request) {
 
     const data = await collection
       .find(query)
+      .project({
+        _id: 1,
+        Supervisor: 1,
+        "#trip": 1,
+        version_type: 1,
+        TotalMinutes: 1,
+        total_engine_on_min_not_plant: 1,
+        Duration_str: 1,
+        Duration_str_not_plant: 1,
+        TruckPlateNo: 1,
+        Date: 1,
+        สำรองเวลาโหลด: 1,
+        ส่วนต่าง: 1,
+        ส่วนต่าง_hhmm: 1,
+        จำนวนลิตร: 1,
+        not_plant_minutes: 1,
+        not_plant_hhmm: 1,
+        not_plant_liter: 1,
+        year: 1,
+        month: 1
+      })
       .sort({ Date: -1, TruckPlateNo: 1 })
       .toArray()
 
